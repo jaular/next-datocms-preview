54fa153431e5bdcac39105f6664ef263f605b66b
José Aular <jose14aular@gmail>
2022-05-23 11:12:05 -0400

    Ignore parameter

--- pages/api/exit-preview.ts
+++ pages/api/exit-preview.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiResponse } from "next";
 
-export default function handler(res: NextApiResponse) {
+export default function handler(_req: unknown, res: NextApiResponse) {
   res.clearPreviewData();
 
   res.writeHead(307, { Location: "/" });
